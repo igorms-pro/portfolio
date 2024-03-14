06a5fe6947b73ddbb9c62a1d3e655311aa90111f
igorms.pro <igor@cometh.io>
2024-03-14 10:01:55 +0100

    up with mobile desktop show hide

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,23 +1,17 @@
 <template lang="">
   <header class="bg-transparent">
     <nav
-      class="sm:p-12 flex items-center justify-center font-normal h-10 text-white text-xl dark:text-gray-light"
+      class="sm:p-10 p-5 font-normal flex h-10 items-center text-white text-xl dark:text-gray-light"
     >
-      <div class="flex flex-1 h-9">
+      <div class="flex flex-1 h-full sm:h-auto">
         <a href="#" class="-m-1.5 p-1.5">
-          <span>© Igor Moreno Semedo</span>
+          <span class="">© Igor Moreno Semedo</span>
         </a>
       </div>
-      <div class="flex sm:hidden">
-        <button
-          type="button"
-          class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5"
-          @click="mobileMenuOpen = true"
-        >
-          <Bars3Icon class="h-6 w-6" aria-hidden="true" />
-        </button>
-      </div>
-      <div class="text-white flex flex-auto items-center space-x-12 h-9">
+      <span @click="mobileMenuOpen = true" class="lg:hidden h-full"> Menu</span>
+      <div
+        class="hidden sm:flex flex-auto text-white items-center space-x-12 h-9"
+      >
         <a href="#about" class="text-sm leading-6">About</a>
         <a href="#skills" class="text-sm leading-6">Skills and Tools</a>
         <a href="#work" class="text-sm leading-6">Work</a>
@@ -36,9 +30,60 @@
         <GlobeAltIcon class="h-6 w-6 cursor-pointer" aria-hidden="true" />
       </div>
     </nav>
-    <!-- Mobile menu, show/hide based on menu open
-  state. -->
-    <div></div>
+    <!-- Mobile menu, show/hide based on menu open  state. -->
+    <Dialog
+      as="div"
+      class="lg:hidden"
+      @close="mobileMenuOpen = false"
+      :open="mobileMenuOpen"
+    >
+      <div class="fixed inset-0 z-10" />
+      <DialogPanel
+        class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
+      >
+        <div class="flex items-center justify-between">
+          <a href="#" class="-m-1.5 p-1.5">
+            <span>© Igor Moreno Semedo</span>
+          </a>
+          <button
+            type="button"
+            class="-m-2.5 rounded-md p-2.5 text-gray-700"
+            @click="mobileMenuOpen = false"
+          >
+            <span class="sr-only">Close menu</span>
+            <XMarkIcon class="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+        <div class="mt-6 flow-root">
+          <div class="-my-6 divide-y divide-gray-500/10">
+            <div class="space-y-2 py-6">
+              <a
+                href="#"
+                class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >Features</a
+              >
+              <a
+                href="#"
+                class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >Marketplace</a
+              >
+              <a
+                href="#"
+                class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >Company</a
+              >
+            </div>
+            <div class="py-6">
+              <a
+                href="#"
+                class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >Log in</a
+              >
+            </div>
+          </div>
+        </div>
+      </DialogPanel>
+    </Dialog>
   </header>
 </template>
 
