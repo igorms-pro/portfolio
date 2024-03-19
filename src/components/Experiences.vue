e9f842affcfdd922f42e4383fce043f4261240fb
igorms.pro <igor@cometh.io>
2024-03-19 09:48:23 +0100

    experiences vue component added v0

---
+++ src/components/Experiences.vue
@@ -0,0 +1,9 @@
+<template>
+  <section class="experience sm:px-12 px-5 h-auto">
+    <Divider label="Experiences" circleClassNames="left-1/2" />
+  </section>
+</template>
+<script setup>
+import Divider from './Divider.astro'
+</script>
+<style lang=""></style>
